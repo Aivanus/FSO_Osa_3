b15c8b732a173b313db88e28f8b8513f4cd588f4
Aivanus <ivank1995@hotmail.com>
2018-07-29 18:03:49 +0300

    3.2 aj 3.3 tehty

--- index.js
+++ index.js
@@ -2,39 +2,55 @@ const express = require('express')
 const app = express()
 
 let persons = [
-	{
-		"name": "Arto Hellas",
-		"number": "040-123456",
-		"id": 1
-	},
-	{
-		"name": "Martti Tienari",
-		"number": "040-123456",
-		"id": 2
-	},
-	{
-		"name": "Arto Järvinen",
-		"number": "040-123456",
-		"id": 3
-	},
-	{
-		"name": "Lea Kutvonen",
-		"number": "040-123456",
-		"id": 4
-	},
-	{
-		"name": "Hello",
-		"number": "1234",
-		"id": 5
-	}
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Martti Tienari",
+    "number": "040-123456",
+    "id": 2
+  },
+  {
+    "name": "Arto Järvinen",
+    "number": "040-123456",
+    "id": 3
+  },
+  {
+    "name": "Lea Kutvonen",
+    "number": "040-123456",
+    "id": 4
+  },
+  {
+    "name": "Hello",
+    "number": "1234",
+    "id": 5
+  }
 ]
 
 app.get('/', (req, res) => {
   res.send('<h1>Persons API</h1>')
 })
 
-app.get('/api/persons', (req, res)=>{
+app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.get('/info', (req, res) => {
+  const date = new Date()
+  res.send('<p>Puhelinluettelossa on ' + persons.length + ' henkilön tiedot</p>' + date)
 })
 
 const PORT = 3001
