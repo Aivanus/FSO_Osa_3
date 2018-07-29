a00bda4e08d4bbc56209771636c7d81bde4746d9
Aivanus <ivank1995@hotmail.com>
2018-07-29 21:53:37 +0300

    ->3.6 tehty

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let persons = [
   {
@@ -51,6 +54,35 @@ app.get('/api/persons/:id', (req, res) => {
 app.get('/info', (req, res) => {
   const date = new Date()
   res.send('<p>Puhelinluettelossa on ' + persons.length + ' henkilön tiedot</p>' + date)
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (persons.find(p => p.name === body.name)) {
+    return res.status(400).json({ error: 'The name must be unique!' })
+  }
+
+  if (body.name === undefined || body.number === undefined) {
+    return res.status(400).json({ error: 'You must specify both name and number' })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 1000000)
+  }
+
+  persons = persons.concat(person)
+
+  res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  res.status(203).end()
 })
 
 const PORT = 3001
